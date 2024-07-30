c658091d68b0e876e57442ce721300e4fcfb8d25
ahmetkasap <aahmetkasap@gmail.com>
2024-07-30 14:58:52 +0300

    docker compose postgresql added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
-
+import { AccountModule } from './modules/account/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config'
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, AccountModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+  }),],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix(`${process.env.NAME}/${process.env.VERSION}`);
+
+  await app.listen(process.env.PORT || 5001);
 }
 bootstrap();
