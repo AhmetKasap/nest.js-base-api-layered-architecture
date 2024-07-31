e311cb06825aa86eab73742da08fef49ab81d496
ahmetkasap <aahmetkasap@gmail.com>
2024-07-31 23:00:20 +0300

    class validator added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 import 'dotenv/config'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
-  app.setGlobalPrefix(`${process.env.NAME}/${process.env.VERSION}`);
+  app.setGlobalPrefix(`${process.env.NAME}/${process.env.VERSION}`)
 
-  await app.listen(process.env.PORT || 5001);
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(process.env.PORT || 5001)
 }
-bootstrap();
+bootstrap()
