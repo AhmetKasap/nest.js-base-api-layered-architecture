65d4e1fb1ab011fac54ca3520de58f4fac783e4d
ahmetkasap <aahmetkasap@gmail.com>
2024-08-01 12:51:00 +0300

    auth service added

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller,Get, Post } from '@nestjs/common';
-import { UserDTO } from '../user/DTO/UserDTO';
+import { UserDTO } from '../user/dto/UserDTO';
 import { AuthService } from './auth.service';
+import {LoginDTO} from './dto/LoginDTO'
 
 @Controller('auth')
 export class AuthController {
@@ -10,4 +11,9 @@ export class AuthController {
     async register(@Body() userDTO : UserDTO): Promise<UserDTO> {
         return await this.authService.register(userDTO)
     }   
+
+    @Post('/login')
+    async login (@Body() loginDTO : LoginDTO) : Promise<LoginDTO> {
+        return await this.authService.login(loginDTO)
+    }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,23 +2,41 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../user/model/UserEntity';
 import { Repository } from 'typeorm';
-import { UserDTO } from '../user/DTO/UserDTO';
+import { UserDTO } from '../user/dto/UserDTO';
 import { ErrorException } from 'src/core/utils/ErrorException';
+import { LoginDTO } from './dto/LoginDTO';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class AuthService {
-    
+
     constructor(
         @InjectRepository(UserEntity)
-        private readonly authRepository : Repository<UserEntity>
-    ){}
+        private readonly authRepository: Repository<UserEntity>
+    ) { }
 
-    async register(userDTO : UserDTO) : Promise<UserEntity> {
-        const existingUser = await this.authRepository.findOne({where : {email : userDTO.email}})
+    async register(userDTO: UserDTO): Promise<UserEntity> {
+        const existingUser = await this.authRepository.findOne({ where: { email: userDTO.email } })
         if (existingUser) throw new ErrorException('User with this email already exists', 409)
 
-        const auth = this.authRepository.create(userDTO)
-        await this.authRepository.save(auth)
+        const hashedPassword = await bcrypt.hash(userDTO.password, 10); // 10, saltRounds değeridir
+        const user = this.authRepository.create({
+            ...userDTO,
+            password: hashedPassword,
+        });
+        await this.authRepository.save(user);
+        return user;
+
+    }
+
+    async login(loginDTO: LoginDTO): Promise<UserEntity> {
+        const auth = await this.authRepository.findOne({ where: { email: loginDTO.email} })
+        if(!auth) throw new ErrorException('Invalid email or password',401) 
+        
+        const isPasswordValid = await bcrypt.compare(loginDTO.password, auth.password)
+        if(!isPasswordValid) throw new ErrorException('Invalid email or password',401) 
+
         return auth
     }
 
